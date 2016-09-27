b6490047c69853870a95081bd37adc5dbf7b1d27
kimlanghy12 <kimlanghy12@gmail.com>
2016-09-27 16:27:56 +0700

    add a lot modules

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ A simple "Hello World" bot for the Microsoft Bot Framework.
 -----------------------------------------------------------------------------*/
 
 var restify = require('restify');
-var builder = require('../../core/');
+var builder = require('botbuilder');
 
 //=========================================================
 // Bot Setup
